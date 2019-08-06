c9a5351704300c9644f094fd3e0727f2e090fee2
oliviaalchemy <olivia@alchemytechs.com>
2019-08-06 15:44:46 +0100

    Add project files.

---
+++ Return Statement/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Return_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cubedNumber = cube(5);
+            Console.WriteLine(cubedNumber);
+            Console.ReadLine();
+        }
+
+        static int cube(int num)
+        {
+            int result = num * num * num;
+            return result; 
+        }
+    }
+}
